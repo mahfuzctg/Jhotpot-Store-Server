d621f75a3d0fd1ccfcb0a11efc2a43f755554fe1
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-02 23:59:51 +0600

    app.ts added

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import httpStatus from "http-status";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
+import router from "./routes";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // Allow requests from this origin
+    credentials: true, // Allow cookies and other credentials
+  })
+);
+app.use(cookieParser());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// router
+app.use("/api", router);
+
+//Testing
+app.get("/", (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to Jhotpot-Store Server!😊",
+  });
+});
+
+//global error handler
+app.use(globalErrorHandler);
+
+//handle non-existing route
+app.use(notFound);
+
+export default app;
