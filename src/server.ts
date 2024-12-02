4bcc59207c3d0e1df78d382ac001f35546f11096
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-03 00:01:02 +0600

    server.ts added

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+import { Server } from "http";
+import app from "./app";
+import config from "./config";
+import { seedSuperAdmin } from "./utils/seed";
+
+async function main() {
+  await seedSuperAdmin();
+  const server: Server = app.listen(config.port, () => {
+    console.log(`🚀 Application is running on port ${config.port}`);
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.info("Server closed!");
+      });
+    }
+    process.exit(1);
+  };
+
+  process.on("uncaughtException", (error) => {
+    console.log(error);
+    exitHandler();
+  });
+
+  process.on("unhandledRejection", (error) => {
+    console.log(error);
+    exitHandler();
+  });
+}
+
+main();
