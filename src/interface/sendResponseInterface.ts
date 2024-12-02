775ae2452075f4b8815c99eda92bb167e3f3d4f7
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-03 01:31:10 +0600

    interfaces--added

---
+++ src/interface/sendResponseInterface.ts
@@ -0,0 +1,15 @@
+export type TMeta = {
+  limit: number;
+  page: number;
+  total: number;
+  totalPage: number;
+};
+
+export type TResponse<T> = {
+  success: boolean;
+  statusCode: number;
+  message?: string;
+  meta?: TMeta;
+  token?: string;
+  data: T;
+};
