97b561a5d0df049455e99f800f16516a2b947ec1
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 19:46:40 +0600

    utils--prisma.ts added

---
+++ src/utils/prisma.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: "event",
+      level: "query",
+    },
+    {
+      emit: "event",
+      level: "error",
+    },
+    {
+      emit: "event",
+      level: "info",
+    },
+    {
+      emit: "event",
+      level: "warn",
+    },
+  ],
+});
+
+export default prisma;
