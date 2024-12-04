430636d08a0515e022c7eeb242e189f39d89e1e1
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:36:35 +0600

    user--user.interface added

---
+++ src/modules/users/user.route.ts
@@ -0,0 +1,39 @@
+import { UserRole } from "@prisma/client";
+import express from "express";
+import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validateRequest";
+import { userController } from "./user.controller";
+import { userValidation } from "./user.validation";
+
+const router = express.Router();
+
+router.get(
+  "/me",
+  auth(
+    UserRole.SUPER_ADMIN,
+    UserRole.ADMIN,
+    UserRole.VENDOR,
+    UserRole.CUSTOMER
+  ),
+  userController.getMyProfile
+);
+
+router.post(
+  "/create-admin",
+  validateRequest(userValidation.createUser),
+  userController.createAdmin
+);
+
+router.post(
+  "/create-vendor",
+  validateRequest(userValidation.createUser),
+  userController.createVendor
+);
+
+router.post(
+  "/create-customer",
+  validateRequest(userValidation.createUser),
+  userController.createCustomer
+);
+
+export const UserRoutes = router;
