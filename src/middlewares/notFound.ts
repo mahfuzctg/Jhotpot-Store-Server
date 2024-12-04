c2011c18cfaa172094f72edab00d282db22a41ba
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:22:24 +0600

    middlewares--notFound.ts modified

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 
+// ! Middleware to handle 404 errors when no matching route is found
 const notFound = (req: Request, res: Response) => {
+  // * Return a 404 response with error details
   return res.status(httpStatus.NOT_FOUND).json({
-    success: false,
-    message: "API NOT FOUND!",
+    success: false, // ? Indicates the response is an error
+    message: "API NOT FOUND!", // ? Main message to inform the user of the error
     error: {
-      path: req.originalUrl,
-      message: "Your requested path is not found!",
+      path: req.originalUrl, // ? Log the path the user tried to access
+      message: "Your requested path is not found!", // Custom message about the error
     },
   });
 };
