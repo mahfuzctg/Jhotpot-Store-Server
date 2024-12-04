23cde888cc34201ed17237e9763e80be57083311
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:44:34 +0600

    users-user.validation added

---
+++ src/modules/users/user.validation.ts
@@ -0,0 +1,25 @@
+import { UserStatus } from "@prisma/client";
+import { z } from "zod";
+
+const createUser = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: "Name is required",
+    }),
+    email: z.string({
+      required_error: "Email is required",
+    }),
+    password: z.string({ required_error: "Password is required" }),
+  }),
+});
+
+const updateStatus = z.object({
+  body: z.object({
+    status: z.enum([UserStatus.ACTIVE, UserStatus.BLOCKED, UserStatus.DELETED]),
+  }),
+});
+
+export const userValidation = {
+  createUser,
+  updateStatus,
+};
