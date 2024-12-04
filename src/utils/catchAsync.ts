455ad7aa00e8be288bb6519e44190fe5c3917fd3
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:12:48 +0600

    utils--catchAsync some changed

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,9 +1,12 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express"; // * Importing necessary types from Express
 
+// !! A higher-order function that wraps an async request handler to catch errors
 const catchAsync = (fn: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+    // * Wrapping the asynchronous request handler in a Promise to catch unhandled errors
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err)); // !! Passing any caught errors to the next middleware (error handler)
   };
 };
 
+// * Exporting the catchAsync function for use in other parts of the application
 export default catchAsync;
