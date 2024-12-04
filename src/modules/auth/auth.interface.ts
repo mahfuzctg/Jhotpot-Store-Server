4e46f2f4f0492f333c0ed64ba4545b5c8de65aba
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 19:45:21 +0600

    modules--auth.interface added

---
+++ src/modules/auth/auth.interface.ts
@@ -0,0 +1,11 @@
+export type TLoginUser = {
+  email: string;
+  password: string;
+};
+
+export type TSocialLoginUser = {
+  name: string;
+  profilePhoto: string;
+  email: string;
+  password?: string;
+};
