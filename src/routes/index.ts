2cfc3c939ec5751c10c27ec09245f35873bcc12c
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:10:16 +0600

    routes--some changed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,18 @@
-import { Router } from "express";
-import { AuthRoutes } from "../modules/Auth/auth.route";
-import { UserRoutes } from "../modules/Users/user.route";
+import { Router } from "express"; // * Importing Router from Express
+import { AuthRoutes } from "../modules/auth/auth.route"; // * Importing authentication routes
 
-const router = Router();
+const router = Router(); // * Initializing the main router
 
+// !! Array to manage module-specific routes
 const moduleRouter = [
   {
-    path: "/auth",
-    route: AuthRoutes,
-  },
-  {
-    path: "/users",
-    route: UserRoutes,
+    path: "/auth", // !! Base path for authentication-related routes
+    route: AuthRoutes, // !! Routes defined in the Auth module
   },
 ];
 
+// !! Dynamically applying all module routes to the main router
 moduleRouter.forEach((route) => router.use(route.path, route.route));
 
+// * Exporting the configured router for use in the main application
 export default router;
