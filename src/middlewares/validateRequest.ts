4b15087ba9c3c4afe97e8bade7674bfd2541eadc
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:23:24 +0600

    middlewares--validate-req modified

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -2,24 +2,29 @@ import { NextFunction, Request, Response } from "express";
 import { AnyZodObject } from "zod";
 import catchAsync from "../utils/catchAsync";
 
+// ! Middleware to validate request bodies using Zod schemas
 const validateRequest = (schema: AnyZodObject) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // * Parse the request body using the provided schema
     await schema.parseAsync({
-      body: req.body,
+      body: req.body, // ? Pass the request body to Zod schema for validation
     });
-    next();
+    next(); // ? If validation is successful, continue to the next middleware
   });
 };
 
+// ! Middleware to validate cookies using Zod schemas
 export const validateRequestCookies = (schema: AnyZodObject) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // * Parse the request cookies using the provided schema
     const parsedCookies = await schema.parseAsync({
-      cookies: req.cookies,
+      cookies: req.cookies, // ? Pass the request cookies to Zod schema for validation
     });
 
+    // * Attach parsed cookies back to the request object
     req.cookies = parsedCookies.cookies;
 
-    next();
+    next(); // ? If validation is successful, continue to the next middleware
   });
 };
 
