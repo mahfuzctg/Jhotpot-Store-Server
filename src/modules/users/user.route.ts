9d89b163a8b47f8f2a64749dbccf514f32a319a7
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:43:43 +0600

    users--user.controller added

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -2,7 +2,8 @@ import { UserRole } from "@prisma/client";
 import express from "express";
 import auth from "../../middlewares/auth";
 import validateRequest from "../../middlewares/validateRequest";
-import { userController } from "./user.controller";
+
+import { userController } from "./auth.controller";
 import { userValidation } from "./user.validation";
 
 const router = express.Router();
