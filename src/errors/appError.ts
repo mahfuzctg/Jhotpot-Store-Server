9c612cc6e74d5e2bfb55d6fedf2225c4f44c3d7f
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-04 20:24:22 +0600

    errors--appErr.ts modified

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -1,14 +1,15 @@
 class AppError extends Error {
-  public statusCode: number;
+  public statusCode: number; // The HTTP status code for the error.
 
   constructor(statusCode: number, message: string, stack = "") {
-    super(message);
-    this.statusCode = statusCode;
+    super(message); // Calls the parent `Error` constructor with the message.
+    this.statusCode = statusCode; // Set the statusCode for the error.
 
+    // If a stack is passed, set it; otherwise, capture the current stack trace.
     if (stack) {
       this.stack = stack;
     } else {
-      Error.captureStackTrace(this, this.constructor);
+      Error.captureStackTrace(this, this.constructor); // Captures the current call stack.
     }
   }
 }
