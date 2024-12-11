c0c47cbabe62cc7fbec4cfbe31259a2612e90586
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-11 07:18:34 +0600

    utils--payment added

---
+++ src/utils/payment.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from 'axios';
+import config from '../config';
+
+export const initiatePayment = async (paymentData: any) => {
+  try {
+    const response = await axios.post(config.payment_url!, {
+      store_id: config.store_id,
+      signature_key: config.signature_key,
+      tran_id: paymentData.transactionId,
+      success_url: `http://localhost:5000/api/payments/confirmation?transactionId=${paymentData.transactionId}&status=success`,
+      fail_url: `http://localhost:5000/api/payments/confirmation?status=failed`,
+      cancel_url: 'http://localhost:3000/',
+      amount: paymentData.amount,
+      currency: 'BDT',
+      desc: 'Merchant Registration Payment',
+      cus_name: paymentData.customerName,
+      cus_email: paymentData.customerEmail,
+      cus_add1: 'N/A',
+      cus_add2: 'N/A',
+      cus_city: 'N/A',
+      cus_state: 'N/A',
+      cus_postcode: 'N/A',
+      cus_country: 'N/A',
+      cus_phone: 'N/A',
+      type: 'json',
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error('Payment initiation failed!');
+  }
+};
+
+export const verifyPayment = async (tnxId: string) => {
+  try {
+    const response = await axios.get(config.payment_verify_url!, {
+      params: {
+        store_id: config.store_id,
+        signature_key: config.signature_key,
+        type: 'json',
+        request_id: tnxId,
+      },
+    });
+
+    return response.data;
+  } catch (err) {
+    throw new Error('Payment validation failed!');
+  }
+};
