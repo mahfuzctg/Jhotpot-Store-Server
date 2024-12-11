1f46b60481df423d19e11ad27a77aa147550ce9d
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-11 09:36:53 +0600

    modules--category-validation updated

--- src/modules/category/category.validation.ts
+++ src/modules/category/category.validation.ts
@@ -1,12 +1,12 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 const createCategoryValidation = z.object({
   body: z.object({
     category: z.string({
-      required_error: "Category Name is required",
+      required_error: 'Category Name is required',
     }),
     image: z.string({
-      required_error: "Category image is required",
+      required_error: 'Category image is required',
     }),
   }),
 });
@@ -15,12 +15,12 @@ const updateCategoryValidation = z.object({
   body: z.object({
     category: z
       .string({
-        required_error: "Category Name is required",
+        required_error: 'Category Name is required',
       })
       .optional(),
     image: z
       .string({
-        required_error: "Category image is required",
+        required_error: 'Category image is required',
       })
       .optional(),
   }),
