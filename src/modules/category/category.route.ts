6392cf597b767e5cf4582afffad0febed7a4f07d
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-11 08:47:12 +0600

    modules--category-route updated

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,26 +1,32 @@
-import { UserRole } from "@prisma/client";
-import express from "express";
-import auth from "../../middlewares/auth";
-import validateRequest from "../../middlewares/validateRequest";
-import { CategoryController } from "./category.controller";
-import { categoryValidation } from "./category.validation";
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
+import { CategoryController } from './category.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { categoryValidation } from './category.validation';
 
 const router = express.Router();
 
 router.post(
-  "/",
+  '/',
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(categoryValidation.createCategoryValidation),
-  CategoryController.createCategory
+  CategoryController.createCategory,
 );
 
-router.get("/", CategoryController.getAllCategories);
+router.get('/', CategoryController.getAllCategories);
 
 router.patch(
-  "/:categoryId",
+  '/:categoryId',
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
   validateRequest(categoryValidation.updateCategoryValidation),
-  CategoryController.updateCategory
+  CategoryController.updateCategory,
+);
+
+router.delete(
+  '/:categoryId',
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  CategoryController.deleteCategory,
 );
 
 export const CategoryRoutes = router;
