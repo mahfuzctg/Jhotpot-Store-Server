55cb7b9e166f6a356c57a41107dd95fb8139aff2
Mahfuz <aamahfuz.pro@gmail.com>
2024-12-13 14:57:01 +0600

    modules--review-service added

---
+++ src/modules/Reviews/review.services.ts
@@ -0,0 +1,106 @@
+import httpStatus from 'http-status';
+import AppError from '../../errors/appError';
+import prisma from '../../utils/prisma';
+import { IAuthUser } from '../Users/user.interface';
+import { TReview } from './review.interface';
+
+const createReview = async (payload: TReview, user: IAuthUser) => {
+  const customer = await prisma.customer.findUnique({
+    where: {
+      email: user?.email,
+      isDeleted: false,
+    },
+  });
+
+  if (!customer) {
+    throw new AppError(httpStatus.NOT_FOUND, "Customer doesn't exist!");
+  }
+
+  const product = await prisma.product.findUnique({
+    where: {
+      id: payload.productId,
+    },
+  });
+
+  if (!product) {
+    throw new AppError(httpStatus.NOT_FOUND, "Product doesn't exist!");
+  }
+
+  const vendor = await prisma.vendor.findUnique({
+    where: {
+      id: payload.vendorId,
+    },
+  });
+
+  if (!vendor) {
+    throw new AppError(httpStatus.NOT_FOUND, "Vendor doesn't exist!");
+  }
+
+  const reviewInfo = { ...payload, customerId: customer.id };
+
+  const result = await prisma.review.create({
+    data: reviewInfo,
+  });
+
+  return result;
+};
+
+const getAllReviews = async (query: Record<string, string>) => {
+  if (!query.productId && !query.vendorId) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'Either productId or vendorId must be provided!',
+    );
+  }
+
+  if (query.productId) {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: query.productId,
+      },
+    });
+
+    if (!product) {
+      throw new AppError(httpStatus.NOT_FOUND, "Product doesn't exist!");
+    }
+
+    return prisma.review.findMany({
+      where: {
+        productId: query.productId,
+      },
+      include: {
+        product: true,
+        customer: true,
+        vendor: true,
+      },
+    });
+  }
+
+  if (query.vendorId) {
+    const vendor = await prisma.vendor.findUnique({
+      where: {
+        id: query.vendorId,
+      },
+    });
+
+    if (!vendor) {
+      throw new AppError(httpStatus.NOT_FOUND, "Vendor doesn't exist!");
+    }
+
+    return prisma.review.findMany({
+      where: {
+        vendorId: query.vendorId,
+      },
+      include: {
+        product: true,
+        customer: true,
+        vendor: true,
+      },
+    });
+  }
+};
+
+export const ReviewServices = {
+  createReview,
+  getAllReviews,
+};
